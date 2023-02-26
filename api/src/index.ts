4362130a9d905a1d335787c7e1b48a44ec6fbd8a
Nie-Mand <chill.out.x19@gmail.com>
2023-02-26 15:30:05 +0100

    check country

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,6 +3,8 @@ import express from 'express'
 import cors from 'cors'
 import { json } from 'body-parser'
 import router from './routes'
+import axios from "axios";
+
 import { initiateDatabase } from './config/db'
 const app = express()
 
@@ -12,6 +14,23 @@ initiateDatabase()
     .then(() => {
         app.use(cors())
         app.use(json())
+        app.use(async (req, res, next) => {
+          const remote_ip =
+            req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+
+          const ctry = await axios
+            .get(`https://api.country.is/${remote_ip}`)
+            .then((res) => res.data)
+            .catch((e) => ({
+              country: "Unknown",
+            }));
+
+          if (ctry.country !== "UK") {
+            return res.status(404).end();
+          }
+
+          next();
+        });
         app.use("/api", router);
         app.listen(3000)
     })
